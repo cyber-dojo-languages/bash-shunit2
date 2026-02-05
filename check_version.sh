84b07330563e0f79501fd49563c66326a433a546
JonJagger <jon@kosli.com>
2026-02-05 11:38:44 +0000

    Add version check

---
+++ check_version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -Eeu
+
+readonly REGEX="image_name\": \"(.*)\""
+readonly JSON=`cat docker/image_name.json`
+[[ ${JSON} =~ ${REGEX} ]]
+readonly IMAGE_NAME="${BASH_REMATCH[1]}"
+
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly EXPECTED=2.1.9
+readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} bash -c "grep '^SHUNIT_VERSION' /usr/bin/shunit2")
+if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
+  echo "VERSION CONFIRMED as ${EXPECTED}"
+else
+  echo "VERSION EXPECTED: ${EXPECTED}"
+  echo "VERSION   ACTUAL: ${ACTUAL}"
+  exit 42
+fi
